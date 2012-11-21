a5ca79c88c0dc52c5d2d036fc333a7254e02893f
Simon Li <spli@dundee.ac.uk>
2012-11-21 18:18:44 +0000

    Generate Gaussian numbers with mu/sigma. Can now generate a configurable number of objects to save. Configurable output/input file.

--- hdf5test.cpp
+++ hdf5test.cpp
@@ -8,6 +8,7 @@
 #include <ctime>
 #include <map>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include <H5Cpp.h>
@@ -24,8 +25,8 @@ public:
     typedef std::vector<Type> Vector;
     static const int RANK = 2;
 
-    HDF5(std::string filename, unsigned int flags = H5F_ACC_TRUNC):
-	_file(filename.c_str(), flags),
+    HDF5(std::string filename, bool overwrite = false):
+	_file(filename.c_str(), overwrite ? H5F_ACC_TRUNC : 0),
 	_storedType(PredType::NATIVE_DOUBLE) {
     }
 
@@ -53,12 +54,34 @@ public:
 
 	DataSet dset = _file.createDataSet(
 	    fullname.c_str(), _storedType, dspace, cparams);
-	//_dsmap[fullname] =  dset;
 	return dset;
     }
 
     DataSet getDataSet(std::string name) {
 	return _file.openDataSet(name.c_str());
+    }
+
+    Group getGroup(std::string name) {
+	return _file.openGroup(name.c_str());
+    }
+
+    struct RC {
+	int r;
+	int c;
+    };
+
+    RC numberOfRowsCols(const DataSet& ds) const {
+	DataSpace space = ds.getSpace();
+	int rank = space.getSimpleExtentNdims();
+	assert(rank == RANK);
+
+	hsize_t dims[2];
+	space.getSimpleExtentDims(dims, NULL);
+
+	RC rc;
+	rc.r = dims[0];
+	rc.c = dims[1];
+	return rc;
     }
 
     void addRow(const Vector& values, DataSet& ds) {
@@ -137,6 +160,9 @@ public:
 	}
     }
 
+    const H5File& getFile() const {
+	return _file;
+    }
 private:
     void printDataSetSize(const DataSet& ds) const {
 	DataSpace space = ds.getSpace();
@@ -155,7 +181,6 @@ private:
     }
 
     H5File _file;
-    std::map<std::string, DataSet> _dsmap;
     const PredType _storedType;
 };
 
@@ -166,7 +191,7 @@ public:
     typedef std::map<std::string, HDF5::Vector*> MapType;
 
     SimulatedData(int id, double mu, double delField = 0.0):
-	_id(id), _timestamp(0) {
+	_id(id), _timestamp(0), _sigma(4) {
 	_ns[0] = 10;
 	_ns[1] = 20;
 	_ns[2] = 30;
@@ -199,10 +224,10 @@ private:
 	    _empty(true) {
 	}
 
-	double next() {
+	double next(double mu, double sigma) {
 	    if (!_empty) {
 		_empty = true;
-		return _y;
+		return _y * sigma + mu;
 	    }
 
 	    // http://en.wikipedia.org/wiki/Marsaglia_polar_method
@@ -214,7 +239,8 @@ private:
 		    double m = std::sqrt(-2.0 * std::log(s) / s);
 		    double x = u * m;
 		    _y = v * m;
-		    return x;
+		    _empty = false;
+		    return x * sigma + mu;
 		}
 	    }
 	}
@@ -227,7 +253,7 @@ private:
     void randN(HDF5::Vector& vs, double mu) {
 	RandomGaussian r;
 	for (HDF5::Vector::iterator it = vs.begin(); it != vs.end(); ++it) {
-	    *it = r.next();
+	    *it = r.next(mu, _sigma);
 	}
     }
 
@@ -259,6 +285,7 @@ private:
     int _id;
     int _timestamp;
     int _ns[4];
+    double _sigma;
 };
 
 void generateData(HDF5& hdf, int n)
@@ -285,8 +312,10 @@ void generateData(HDF5& hdf, int n)
 
     for (int i = 0; i < n; ++i)
     {
+	double mus[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
 	std::cout << i << " ";
-	SimulatedData sd(i, 0.0);
+	SimulatedData sd(i, mus[i % 10]);
 	SimulatedData::MapType data = sd.getData();
 	for (SimulatedData::MapType::const_iterator it = data.begin();
 	     it != data.end(); ++it)
@@ -298,6 +327,96 @@ void generateData(HDF5& hdf, int n)
 	}
     }
     std::cout << std::endl;
+}
+
+
+struct DSAdder
+{
+    HDF5* hdf;
+    double total;
+    std::string path;
+};
+
+// An operator for use with H5Giterate
+herr_t datasetAdder(hid_t gid, const char* name, void* data)
+{
+    DSAdder* d = static_cast<DSAdder*>(data);
+    std::string prevPath = d->path;
+    d->path = d->path + "/" + name;
+
+    herr_t r = 0;
+    H5G_stat_t stat;
+    d->hdf->getFile().getObjinfo(d->path.c_str(), true, stat);
+
+    switch (stat.type)
+    {
+    case H5G_GROUP:
+    {
+	std::cout << "Group: " << d->path << std::endl;
+	Group group = d->hdf->getGroup(d->path);
+	r = H5Giterate(group.getId(), d->path.c_str(), NULL,
+		       datasetAdder, data);
+	break;
+    }
+
+    case H5G_DATASET:
+    {
+	std::cout << "DataSet: " << d->path << std::endl;
+
+	DataSet ds = d->hdf->getDataSet(d->path);
+	HDF5::Vector values;
+
+	HDF5::RC rc = d->hdf->numberOfRowsCols(ds);
+
+	double total = 0;
+
+	for (int r = 0; r < rc.r; ++r)
+	{
+	    d->hdf->getRow(ds, values, r);
+	    for (HDF5::Vector::const_iterator it = values.begin();
+		 it != values.end(); ++it)
+	    {
+		total += *it;
+	    }
+	}
+
+	std::cout << "\tRows: " << rc.r << " Cols:" << rc.c
+		  << " Table total:" << total << std::endl;
+	d->total += total;
+	break;
+    }
+
+    case H5G_LINK:
+	std::cout << "Link: " << d->path << " (ignoring)" << std::endl;
+	break;
+
+    case H5G_TYPE:
+	std::cout << "Type: " << d->path << " (ignoring)" << std::endl;
+	break;
+
+    default:
+	std::cerr << "Unexpected type: " << d->path << std::endl;
+	return -1;
+    }
+
+    d->path = prevPath;
+    return 0;
+}
+
+void readData(HDF5& hdf)
+{
+    DSAdder data;
+    data.hdf = &hdf;
+    data.total = 0;
+    data.path = "";
+
+    herr_t r = datasetAdder(hdf.getFile().getId(), "",
+			    static_cast<void*>(&data));
+    if (r < 0)
+    {
+	std::cout << "Error occurred: " << r << std::endl;
+    }
+    std::cout << "Grand total: " << data.total << std::endl;
 }
 
 void writeHdf5(HDF5& hdf, int nrows, int ncols)
@@ -349,43 +468,94 @@ void readHdf5(HDF5& hdf, int nrows, int ncols)
 }
 
 
-int main(int c, char* argv[])
+void usage(std::string progname)
+{
+    size_t p = progname.find_last_of("/\\");
+    p = (p == std::string::npos)? 0 : p + 1;
+    std::cerr << "Usage: " << progname.substr(p) << " 0|1|N [filename]\n"
+	      << "0 Read objects\n"
+	      << "1 Write a 20,000 row by 2,000 column table\n"
+	      << "N Write N multi-feature multi-table objects (N:integer)\n"
+	      << "filename Default: hdf5-cpp-test.h5\n"
+	      << std::endl;
+}
+
+
+int main(int argc, char* argv[])
 {
     using std::cout;
     using std::cerr;
     using std::endl;
     using std::srand;
 
-    bool writeRead = false;
-
-    int nrows = 20000;
-    int ncols = 2000;
-    int ndata = 10000;
-
     srand(time(NULL));
 
-    std::clock_t start = clock();
-
-    HDF5 hdf("hdf5-cpp-test.h5");
-
-    if (writeRead)
+    if (argc < 2 || argc > 3)
     {
+	usage(argv[0]);
+	return 2;
+    }
+
+    std::istringstream iss(argv[1]);
+    int mode;
+
+    iss >> mode;
+    if (!iss || mode < 0)
+    {
+	cerr << "Invalid argument" <<endl;
+	usage(argv[0]);
+	return 2;
+    }
+
+    std::string fname("hdf5-cpp-test.h5");
+    if (argc == 3)
+    {
+	fname = argv[2];
+    }
+    cerr << "Using file \"" << fname << "\"" << endl;
+
+    std::clock_t start = clock();
+    cerr << "Timing started" << endl;
+
+    if (mode == 0)
+    {
+	bool overwrite = false;
+	HDF5 hdf(fname, overwrite);
+
+	readData(hdf);
+	cerr << "Reading time: "
+	     << static_cast<double>(clock() - start) / CLOCKS_PER_SEC
+	     << "s" << endl;
+    }
+    else if (mode == 1)
+    {
+	int nrows = 20000;
+	int ncols = 2000;
+	bool overwrite = true;
+
+	HDF5 hdf(fname, overwrite);
+
 	writeHdf5(hdf, nrows, ncols);
-	cerr << "Writing: "
+	cerr << "Writing time: "
 	     << static_cast<double>(clock() - start) / CLOCKS_PER_SEC 
 	     << "s" << endl;
 
-	start = clock();
+	/*start = clock();
 
 	readHdf5(hdf, nrows, ncols);
 	cerr << "Reading: "
 	     << static_cast<double>(clock() - start) / CLOCKS_PER_SEC
-	     << "s" << endl;
+	     << "s" << endl;*/
     }
     else
     {
+	int ndata = mode;
+	bool overwrite = true;
+
+	HDF5 hdf(fname, overwrite);
+
 	generateData(hdf, ndata);
-	cerr << "Saved " << ndata << ": "
+	cerr << "Writing " << ndata << " objects, time: "
 	     << static_cast<double>(clock() - start) / CLOCKS_PER_SEC
 	     << "s" << endl;
     }
